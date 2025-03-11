83cd9667901c3645c5a2984c94f9dbe229059e56
Vishwa GW <136871744+vishwagw@users.noreply.github.com>
2025-03-12 02:51:38 +0530

    Update nltk_test.py

--- nltk_test.py
+++ nltk_test.py
@@ -1,2 +1,13 @@
 # using nltk library instead of Deep learning model:
-
+import speech_recognition as sr
+import requests
+from bs4 import BeautifulSoup
+import re
+import pyttsx3
+import logging
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
